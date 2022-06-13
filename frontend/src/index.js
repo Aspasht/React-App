fea4e3f135bd0951e3c0c133a5b327dcc37cfa15
Aspasht <sykoman944@gmail.com>
2022-06-13 13:30:06 +0300

    added full frontend files

---
+++ frontend/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+//http://www.omdbapi.com/?apikey=6db0599d
+
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
+
+
+
+
+
+
+
+
